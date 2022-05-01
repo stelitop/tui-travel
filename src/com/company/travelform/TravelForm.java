c6cd13f030dcb31667318afcc90d6464c5909c58
Stiliyan Nanovski <S.I.Nanovski@student.tudelft.nl>
2022-05-01 11:03:14 +0200

    Add a const list of activities as strings.

--- src/com/company/travelform/TravelForm.java
+++ src/com/company/travelform/TravelForm.java
@@ -1,6 +1,7 @@
 package com.company.travelform;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class TravelForm {
@@ -10,6 +11,18 @@ public class TravelForm {
 	public Climate climate;
 	public TravelMethod travelMethod;
 	public Set<Activity> chosenActivities;
+
+	public static final List<String> activities = List.of(
+			"Visit Museums",
+			"Go Shopping",
+			"Visit Theaters",
+			"Visit Monuments",
+			"Go to the Beach",
+			"Visit Zoos",
+			"Go Clubbing",
+			"Visit Art Galleries",
+			"Beauty and Spa"
+	);
 
 	public TravelForm() {
 		budget = 0.0f;
